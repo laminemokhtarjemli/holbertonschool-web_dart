c053db34e5d7dedb5aeafde044ff7cac6af15939
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-31 21:18:07 +0100

    Delete 1-get_user_id.dart

--- 0x03-asynchronous/1-get_user_id.dart
+++
@@ -1,9 +0,0 @@
-import '1-main.dart';
-import 'dart:convert';
-
-
-Future<String> getUserId() async {
-  final String userData = await fetchUserData();
-  final Map<String, dynamic> user = jsonDecode(userData);
-  return user['id'];
-}
